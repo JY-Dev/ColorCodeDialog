021c3bf0aca345daedd869b8b4681d0088dac5eb
jaeyoung <kjy0302014@gmail.com>
2020-05-21 10:20:29 +0900

    Initial commit

---
+++ app/src/main/java/jaeyoung/kim/colordialog/MainActivity.kt
@@ -0,0 +1,19 @@
+package jaeyoung.kim.colordialog
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        test.setOnClickListener {
+            ColorDialog(this,{ R,B,C,Code ->
+                Toast.makeText(this,Code,Toast.LENGTH_SHORT).show()
+            },0,0,0)
+        }
+    }
+}
